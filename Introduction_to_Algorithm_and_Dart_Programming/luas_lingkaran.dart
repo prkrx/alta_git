65db303ddfac26401c698d14bcd185115eeb51a1
Richo Wardiyanto <38392904+prkrx@users.noreply.github.com>
2022-08-29 22:33:30 +0700

    penambahan luas lingkaran

---
+++ Introduction_to_Algorithm_and_Dart_Programming/luas_lingkaran.dart
@@ -0,0 +1,7 @@
+void main() {
+  double pi = 3.14;
+  int r = 2;
+  var area = pi * r * r;
+  print("hasil dari menghitung luas lingkarang adalah " + area.toStringAsFixed(3)); // arti dari toString itu adalah menambahkan angka di belakang koma
+  
+}
